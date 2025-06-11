2c24dcc74da8b0146a3215e52d3c07de77c1c811
kossanzhar <kossanzhik05@gmail.com>
2025-06-12 00:28:03 +0500

    asdf

--- src/main/java/pet/project/numapapi/controller/PlaceController.java
+++ src/main/java/pet/project/numapapi/controller/PlaceController.java
@@ -1,6 +1,5 @@
 package pet.project.numapapi.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +10,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/places")
-@RequiredArgsConstructor
 public class PlaceController {
     private final PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
     @GetMapping("/all")
     public List<Place> getAllPlaces(){
         return placeService.getAllPlaces();
--- src/main/java/pet/project/numapapi/service/PlaceService.java
+++ src/main/java/pet/project/numapapi/service/PlaceService.java
@@ -1,9 +1,6 @@
 package pet.project.numapapi.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pet.project.numapapi.entity.Place;
 import pet.project.numapapi.entity.repository.PlaceRepository;
@@ -11,9 +8,13 @@ import pet.project.numapapi.entity.repository.PlaceRepository;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class PlaceService{
     private final PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
     public List<Place> getAllPlaces() {
         return placeRepository.findAll();
     }
